febb1f8a6990f561c29222bd310e472933056058
Kat.Lover <jgumiela7@gmail.com>
2024-12-04 07:37:19 -0300

    Dominio

---
+++ Web_Promo/Dominio/Imagen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Imagen
+    {
+        public int IDImagen { get; set; }
+        public int IDArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+
+        public override string ToString()
+        {
+            return ImagenUrl;
+        }
+
+    }
+}
